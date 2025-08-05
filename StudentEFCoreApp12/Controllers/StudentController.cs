5afcdcd46448f90614e4ae2e6db74b6e533dc00e
Dikshan kc <146184803+Dikshan23@users.noreply.github.com>
2025-08-05 20:47:30 +0545

    Update StudentController.cs

--- StudentEFCoreApp12/Controllers/StudentController.cs
+++ StudentEFCoreApp12/Controllers/StudentController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using StudentEFCoreApp12.Data;
 using StudentEFCoreApp12.Models;
 
@@ -7,18 +7,22 @@ namespace StudentEFCoreApp12.Controllers
     public class StudentController : Controller
     {
         private readonly AppDbContext _context;
+
         public StudentController(AppDbContext context)
         {
             _context = context;
         }
 
+        // GET: /Student/
         public IActionResult Index()
         {
             return View(_context.Students.ToList());
         }
 
+        // GET: /Student/Create
         public IActionResult Create() => View();
 
+        // POST: /Student/Create
         [HttpPost]
         public IActionResult Create(Student student)
         {
@@ -27,12 +31,17 @@ namespace StudentEFCoreApp12.Controllers
             return RedirectToAction("Index");
         }
 
+        // GET: /Student/Edit/5
         public IActionResult Edit(int id)
         {
             var student = _context.Students.Find(id);
+            if (student == null)
+                return NotFound();
+
             return View(student);
         }
 
+        // POST: /Student/Edit
         [HttpPost]
         public IActionResult Edit(Student student)
         {
@@ -41,17 +50,37 @@ namespace StudentEFCoreApp12.Controllers
             return RedirectToAction("Index");
         }
 
+        // GET: /Student/Delete/5
         public IActionResult Delete(int id)
         {
             var student = _context.Students.Find(id);
+            if (student == null)
+                return NotFound();
+
+            return View(student); // This shows Delete.cshtml for confirmation
+        }
+
+        // POST: /Student/DeleteConfirmed
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+                return NotFound();
+
             _context.Students.Remove(student);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        // GET: /Student/Details/5
         public IActionResult Details(int id)
         {
             var student = _context.Students.Find(id);
+            if (student == null)
+                return NotFound();
+
             return View(student);
         }
     }
